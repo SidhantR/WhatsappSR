e3289feb672193f49f33171adf1dc46e1f5ecbda
Sidhant <sidhantrc02@gmail.com>
2021-03-28 19:48:43 +0530

    done

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,12 @@ import "firebase/auth"
 import "firebase/firestore";
 
 const firebaseApp = firebase.initializeApp({
-  apiKey: "AIzaSyBlGfvMT8fI1u8hyP2ZoCtdGj0jC4IuITI",
-  authDomain: "instagram-clone-a9e94.firebaseapp.com",
-  projectId: "instagram-clone-a9e94",
-  storageBucket: "instagram-clone-a9e94.appspot.com",
-  messagingSenderId: "370251323836",
-  appId: "1:370251323836:web:093931ed6e78fca899f3b9",
-  measurementId: "G-L8GNJC3WS5"
+  apiKey: "AIzaSyC21qTaQneaMAl_6R1GVGbZQQDUR-QPbWs",
+  authDomain: "whatsapp-clone-8a13b.firebaseapp.com",
+  projectId: "whatsapp-clone-8a13b",
+  storageBucket: "whatsapp-clone-8a13b.appspot.com",
+  messagingSenderId: "227944634666",
+  appId: "1:227944634666:web:2c701805feef1599bd6653"
 });
 // passing the config object storing in firebaseApp object 
 const db = firebaseApp.firestore();
